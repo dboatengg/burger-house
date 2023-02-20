f8e99f4bebdb5314a057c92ce0162e4a7e8a7d38
dboatengg <dicksonboateng67@gmail.com>
2023-02-20 16:50:51 +0000

    Completed about section with relevant information and added appropriate images

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about__content container">
+        <div className="about__one">
+          <h2>
+            <span className="try">TRY IT TODAY</span>
+            <span>MOST POPULAR BURGER</span>
+          </h2>
+        </div>
+        <div className="about__two">
+          <h2>
+            <span className="try">TRY IT TODAY</span> <span>MOST FUN</span>{" "}
+            <span>MORE TASTE</span>
+          </h2>
+        </div>
+        <div className="about__three">
+          <h2>
+            <span className="try">TRY IT TODAY</span>{" "}
+            <span>FRESH & CHILLI</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
