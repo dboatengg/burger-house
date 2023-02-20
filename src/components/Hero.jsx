29a05cf08ae6beec89cb095bc5d28693d69d1126
dboatengg <dicksonboateng67@gmail.com>
2023-02-20 14:33:04 +0000

    Completed project setup and added header

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import heroImage from "../assets/hero__img.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero__content container">
+        <div className="hero__text">
+          <p>It is a good time for the great taste of burgers</p>
+          <h1>BURGER</h1>
+          <h2>WEEK</h2>
+        </div>
+        <div className="hero__image">
+          <img src={heroImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import logo from "../assets/logo.png";
+import express from "../assets/express.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navbar = () => {
+  return (
+    //prettier-ignore
+    <div className="navbar container">
+      <div className="navbar__mobile">
+        <a href="#" className="logo"><img src={logo} alt="" /></a>
+        <button className="navbar__toggle"><RxHamburgerMenu /></button>
+      </div>
+      <div className="navbar__menu">
+        <div className="top">
+            <img src={express} alt="" />
+            <p>Express Delivery +1 234 567 890</p>
+        </div>
+        <ul className="menu">
+            <li><a href="">HOME</a></li>
+            <li><a href="">MENU</a></li>
+            <li><a href="">OUR STORY</a></li>
+            <li><a href="">CONTACT US</a></li>
+        </ul>
+      </div>
+    </div>
+    //prettier-ignore
+  );
+};
+
+export default Navbar;
