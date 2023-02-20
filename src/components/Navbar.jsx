4d4a9779ffcc9cf2edcdde85bdd77e93b903a228
dboatengg <dicksonboateng67@gmail.com>
2023-02-20 23:11:25 +0000

    Add functionality to toggle navigation menu with hamburger button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
+
 import logo from "../assets/logo.png";
 import express from "../assets/express.png";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleClick = () => {
+    setShowNav(!showNav);
+  };
   return (
     //prettier-ignore
     <div className="navbar">
         <div className="navbar__content container">
           <div className="navbar__mobile">
             <a href="#" className="logo"><img src={logo} alt="" /></a>
-            <button className="navbar__toggle"><RxHamburgerMenu /></button>
+            <button className="navbar__toggle" onClick={handleClick}><RxHamburgerMenu /></button>
           </div>
-          <div className="navbar__menu">
+          <div className={`navbar__menu ${showNav ? 'show__nav':'hide__nav'}`}>
             <div className="top">
                 <img src={express} alt="" />
                 <p>Express Delivery +1 234 567 890</p>
