2d53251e32ed415ad80a4b6df31ccad0d1f5b648
dboatengg <dicksonboateng67@gmail.com>
2023-02-21 09:41:54 +0000

    fix build issue

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,5 @@
-import bottle from "../assets/bottle.png";
+// import bottle from "../assets/bottle.png";
 import plate from "../assets/plate.png";
-// import bottle from '../assets/bottle.png';
 
 const Form = () => {
   return (
@@ -11,7 +10,7 @@ const Form = () => {
           <h2>BOOK YOUR TABLE</h2>
         </div>
         <div className="form__images">
-          <img className="bottle" src={bottle} alt="" />
+          {/* <img className="bottle" src={bottle} alt="" /> */}
           <img className="plate" src={plate} alt="" />
         </div>
         <form className="form__block">
